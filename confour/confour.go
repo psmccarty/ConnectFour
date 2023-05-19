36d59b809558447a6eff5fa4c599d33313f11c3e
Patrick McCarty <psmccarty@wisc.edu>
2023-05-19 18:24:18 -0500

    Improve controlflow

--- confour/confour.go
+++ confour/confour.go
@@ -7,8 +7,8 @@ import (
 	"psmccarty.com/gamelogic"
 )
 
-const NUM_ROWS int = 6
-const NUM_COLS int = 7
+ const NUM_ROWS int = 6
+ const NUM_COLS int = 7
 const PLAYER1 byte = 'X'
 const PLAYER2 byte = 'O'
 
@@ -37,13 +37,15 @@ func main() {
 		result := gameBoard.Outcome(x, y)
 
 		if result == 0 {
+			fmt.Println(gameBoard.String())
 			fmt.Println("Its a tie!")
 			break
 		} else if result == 1 {
-			fmt.Printf("Player %q wins!\n", gameBoard.CurrPlayer)
+			fmt.Println(gameBoard.String())
+			fmt.Printf("Player %q wins!\n", gameBoard.Players[gameBoard.CurrPlayer])
 			break
 		}
-
+		gameBoard.NextPlayer()
 	}
 }
 
